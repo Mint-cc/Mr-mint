5a2bfc2422f1f93184b845f8e4607ffeefa68fa2
Mint-cc <144632767+Mint-cc@users.noreply.github.com>
2023-09-11 17:35:27 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,58 +1,28 @@
 from flask import Flask, request, jsonify
 import datetime
-import json
 
+app = Flask("my_app")  #  "my_app"  name of Flask application
 
-app = Flask(__name__)
-
-# Define the route for the endpoint
 @app.route('/api', methods=['GET'])
 def get_info():
-    # Get query parameters
-    slack_name = request.args.get('slack_name')
-    track = request.args.get('track')
-
-    # Validate that both query parameters are provided
-    if not slack_name or not track:
-        return jsonify({"error": "Both slack_name and track are required."}), 400
-
-    # Get the current day of the week in full
-    current_day_of_week = datetime.datetime.utcnow().strftime('%A')
-
-    # Get the current UTC time
+    slack_name = "Mr Mint"  # Set the Slack name to "Mr Mint"
+    track = "backend"  # Set the track to "backend"
+    current_day = datetime.datetime.now().strftime('%A')
     utc_time = datetime.datetime.utcnow().strftime('%Y-%m-%dT%H:%M:%SZ')
+    github_file_url = "https://github.com/Mint-cc/Mr-mint/blob/main/app.py"
+    github_repo_url = " https://github.com/Mint-cc/Mr-mint"
 
-    # GitHub repo URL and file URL
-    github_repo_url = 'https://github.com/Mint-cc/Mr-mint.git'
-    github_file_url = 'https://github.com/Mint-cc/Mr-mint/blob/main/main.py'
-
-    # Construct the original response JSON
-    original_response = {
-        'Slack Name': "Mr mint",
-        'Current Day of the Week': "Monday",
-        'Current UTC Time': "2023-09-11T04:58:01Z",
-        'Track': "backend",
-        'GitHub File URL': "https://github.com/Mint-cc/Mr-mint/blob/main/main.py",
-        'GitHub Repo URL': "https://github.com/Mint-cc/Mr-mint.git",
-        'Status Code': 200
+    response_data = {
+        "slack_name": slack_name,
+        "current_day": current_day,
+        "utc_time": utc_time,
+        "track": track,
+        "github_file_url": github_file_url,
+        "github_repo_url": github_repo_url,
+        "status_code": 200
     }
 
-    # Create a new dictionary with rearranged keys
-    new_response = {
-    "slack_name": original_response["Slack Name"],
-    "current_day": original_response["Current Day of the Week"],
-    "utc_time": original_response["Current UTC Time"],
-    "track": original_response["Track"],
-    "github_file_url": original_response["GitHub File URL"],
-    "github_repo_url": original_response["GitHub Repo URL"],
-    "status_code": original_response["Status Code"]
-     }
-     
-    # Convert the new_response dictionary to JSON
-    new_response_json = json.dumps(new_response, indent=2)
-
-    # Send the JSON response
-    return new_response_json, 200, {'Content-Type': 'application/json'}
+    return jsonify(response_data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
